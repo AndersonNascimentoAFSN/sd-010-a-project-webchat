02fdc25c4e2b8cabf713cd860058ff76a38e923a
thalysmcarrara <thalysmatiascarrara@gmail.com>
2021-10-30 17:17:39 -0400

    requirement 2 done

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,38 +1,47 @@
 const socket = window.io();
-    const buttonMessage = document.getElementById('send-message');
-    const buttonNickname = document.getElementById('nickname-button');
+const buttonMessage = document.getElementById('send-message');
+const buttonNickname = document.getElementById('nickname-button');
 
-    buttonNickname.addEventListener('click', (_e) => {
-      const nicknameInput = document.getElementById('nickname-input');
-      if (!nicknameInput.value) {
-        alert('O nickname não pode ser vazio');
-      } else {
-        sessionStorage.setItem('nickname', nicknameInput.value);
-      }
-    });
+buttonNickname.addEventListener('click', (_e) => {
+  const nicknameInput = document.getElementById('nickname-input');
+  if (!nicknameInput.value) {
+    alert('O nickname não pode ser vazio');
+  } else {
+    sessionStorage.setItem('nickname', nicknameInput.value);
+  }
+});
 
-    buttonMessage.addEventListener('click', (_e) => {
-      const messageInput = document.getElementById('input-message');
+buttonMessage.addEventListener('click', (_e) => {
+  const messageInput = document.getElementById('input-message');
 
-      const nickname = sessionStorage.getItem('nickname');
-      if (!nickname) {
-        alert('Você deve adicionar um nickname antes de enviar a mensagem!!');
-        return;
-      }
+  const nickname = sessionStorage.getItem('nickname');
+  if (!nickname) {
+    alert('Você deve adicionar um nickname antes de enviar a mensagem!!');
+    return;
+  }
 
-      if (!messageInput.value) {
-        alert('A messagem não pode ser vazia');
-        return;
-      } 
-      
-      socket.emit('message', { nickname, chatMessage: messageInput.value });
-    });
+  if (!messageInput.value) {
+    alert('A messagem não pode ser vazia');
+    return;
+  } 
+  
+  socket.emit('message', { nickname, chatMessage: messageInput.value });
+});
 
-    const createMessage = (message) => {
-      const messagesUl = document.querySelector('#messages');
-      const li = document.createElement('li');
-      li.innerText = message;
-      messagesUl.appendChild(li);
-    };
+const createMessage = (message) => {
+  const messagesUl = document.querySelector('#messages');
+  const li = document.createElement('li');
+  li.innerText = message;
+  li.setAttribute('data-testid', 'message');
+  messagesUl.appendChild(li);
+};
 
-    socket.on('message', (message) => createMessage(message));
+socket.on('connect', () => {
+  const userTitle = document.querySelector('#user-nickname');
+  const nickname = socket.id;
+  const nicknameFormated = nickname.slice(0, 16);
+  sessionStorage.setItem('nickname', nicknameFormated);
+  userTitle.innerText = nicknameFormated;
+});
+
+socket.on('message', (message) => createMessage(message));
